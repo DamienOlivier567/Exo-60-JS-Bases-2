5193c2eb474790b331121cf8507082f22821a393
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2020-12-03 11:51:28 +0100

    Exo-60

---
+++ exo 1/appexo1.js
@@ -0,0 +1,6 @@
+let objet = document.getElementsByTagName('script');
+document.setAttribute("objet", "circle");
+
+let listi = document.getElementsByTagName("li");
+listi.innerHTML = "blabla";
+console.log(listi);
